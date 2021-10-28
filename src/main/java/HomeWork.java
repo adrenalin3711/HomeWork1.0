5bb7f2614e8451b0f3ee15f999e3eeeda65accae
Artem <art.teselcko@yandex.ru>
2021-10-28 19:33:27 +0300

    comments

--- src/main/java/HomeWork.java
+++ src/main/java/HomeWork.java
@@ -1,51 +1,71 @@
+import java.util.Scanner;
+
 public class HomeWork {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+        onValue();
+        inValueNatural();
+        inValueNaturalReverse();
+        printStringAndInt();
+        yearVisokosniyOrNeVisokosniy();
 
     }
 
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-
-    public static int checkSumSign() {
-        int a = -1;
-        int b = -5;
-        int sum = a + b;
-        if (sum >= 0) {
-            System.out.println("Сумма положительная " + sum);
+    public static boolean onValue() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        if (c >= 10 && c <= 20) {
+            System.out.println("within " + " " + c);
+            return true;
         } else {
-            System.out.println("Сумма отрицательная" + sum);
+            System.out.println("no within " + " " + c);
+            return false;
         }
-        return sum;
+
     }
 
-    public static void printColor() {
-        int value = 101;
-        if (value <= 0) {
-            System.out.println("Красный " + value);
-        } else if ((value > 0) && (value <= 100)) {
-            System.out.println("Желтый " + value);
+
+    public static void inValueNatural() {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        if (b >= 0) {
+            System.out.println("Number" + " " + b + " is positive");
         } else {
-            System.out.println("Зеленый " + value);
+            System.out.println("Number" + " " + b + " is not positive");
         }
     }
 
-    public static void compareNumbers() {
-        int a = 22;
-        int b = 38;
-        if (a >= b) {
-            System.out.println("a >= b");
+    public static boolean inValueNaturalReverse() {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        if (b > 0) {
+            System.out.println("number is positiv " + b);
+            return false;
         } else {
-            System.out.println("a < b");
-
+            System.out.println("number is not positiv " + b);
+            return true;
         }
     }
 
+    public static void printStringAndInt() {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int b = sc.nextInt();
+        for (int i = 0; i < b; i++) {
+            System.out.println(a);
+        }
+    }
 
+    public static boolean yearVisokosniyOrNeVisokosniy() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a % 400 == 0 || (a % 4 == 0 && a % 100 != 0)) {
+            System.out.println("Is leap Year ");
+            return true;
+        } else {
+            System.out.println("Is not leap Year ");
+            return false;
+        }
+    }
 }
